a1fe8b93bed5683ec703a470812ee69dc043a56d
Tron89 <claudiuacircota@gmail.com>
2023-12-05 16:48:17 +0100

    It detects where is the part of game

--- eyes.py
+++ eyes.py
@@ -27,33 +27,102 @@ class process_image():
         a, b, c = self.detect_from_minesweeper(minesweeper)
         cv2.waitKey(0)
 
+    # HACK: All this have horrible names, I may sove it
     def detect_from_minesweeper(self, minesweeper):
 
         # Find the counter of mines and time :)
-        def mines_time(minesweeper_preproccesed):
+        def counters(minesweeper):
             save = []
-            # It is preproccesed here to a better handling of the tresh
             minesweeper_preproccesed = self.preprocess(minesweeper, 130)
             contours, _ = cv2.findContours(minesweeper_preproccesed, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-            cv2.imshow("preprocesed minesweeper", minesweeper_preproccesed)
+            cv2.imshow("counters preprocesed minesweeper", minesweeper_preproccesed)
+            
             for i, contour in enumerate(contours):
-                # Takes the area of the contour
                 epsilon = cv2.arcLength(contour, True)
-                # And it approximates the vertices
                 approx = cv2.approxPolyDP(contour, 0.01 * epsilon, True)
                 # It get all it is a quadrilateral decently large
                 if len(approx) == 4 and epsilon >= 100:
-                    # Store it
                     save.append(contour)
 
             # HACK: More for a good debug :)
             cv2.drawContours(minesweeper, save, -1, (0,255,0), 3)
             cv2.imshow("contour minesweeper", minesweeper)
-            cv2.waitKey(0)
             return save[0], save[1]
+        
+        # The width and it's position
+        def game_location(minesweeper):
+            savex = []
+            savey = []
+            minesweeper_preproccesed = self.preprocess(minesweeper, 150)
+            contours, _ = cv2.findContours(minesweeper_preproccesed, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+            cv2.imshow("game preprocesed minesweeper", minesweeper_preproccesed)
+            
+            # Get the first value and he is the max y objective
+            # The first should be the first right-down cell
+
+            # And gets the last line of the minseweeper
+
+            first = contours[0]
+            epsilon = cv2.arcLength(first, True)
+            approx = cv2.approxPolyDP(first, 0.01 * epsilon, True)
+            
+            max_y_first = np.argmin(approx[:, :, 1])
+            coord_max_y_first = approx[max_y_first][0][1]
+            epsilon_aprox = epsilon
+            print(epsilon_aprox)
+            
+            for i, contour in enumerate(contours):
+                epsilon = cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, 0.01 * epsilon, True)
+            
+                max_y = np.argmin(approx[:, :, 1])
+                coord_max_y = approx[max_y][0][1]
+                
+                # It get if is arround the first value max
+                if coord_max_y >= coord_max_y_first - 5 and coord_max_y <= coord_max_y_first + 5 and epsilon >= epsilon_aprox - 5 and epsilon <= epsilon_aprox + 5:
+                    savex.append(contour)
+
+                    # HACK
+                    cv2.drawContours(minesweeper, contours, i, (0,255,0), 3)
+                    cv2.imshow("game minesweeper", minesweeper) 
+            
+            # The max x of the last line
+            # Whith that it gets the last column
+
+            # Joints all the contours
+            all_savex = np.concatenate(savex, axis=0)
+        
+            max_x_last = np.argmax(all_savex[:, :, 0])
+            coord_max_x_last = all_savex[max_x_last][0][0]
+
+            for i, contour in enumerate(contours):
+
+                epsilon = cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, 0.01 * epsilon, True)
+                
+                max_x = np.argmax(approx[:, :, 0])
+                coord_max_x = approx[max_x][0][0]
+                
+                if coord_max_x >= coord_max_x_last - 5 and coord_max_x <= coord_max_x_last + 5 and epsilon >= epsilon_aprox - 5 and epsilon <= epsilon_aprox + 5:
+                    savey.append(contour)
+
+                    # HACK
+                    cv2.drawContours(minesweeper, contours, i, (0,255,0), 3)
+                    cv2.imshow("game minesweeper", minesweeper)
+
+            return savex, savey
+
+
 
         # Main function
-        time_counter, mines_counter = mines_time(minesweeper)
+        time_counter, mines_counter = counters(minesweeper.copy())
+        
+        max_y = np.argmax(mines_counter[:, :, 1])
+        coord_max_y = mines_counter[max_y][0][1]
+
+        game_part = minesweeper.copy()[coord_max_y + 5:,:]
+
+        savex, savey = game_location(game_part)
         a = 1
         b = 2
         c = 3
