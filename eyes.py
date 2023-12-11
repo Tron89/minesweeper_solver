201503a86c02e4ed09a644cd6e72f05426807d98
Tron89 <claudiuacircota@gmail.com>
2023-12-11 20:18:42 +0100

    It gets where exactly of the game and some things more

--- eyes.py
+++ eyes.py
@@ -9,6 +9,12 @@ import numpy as np
 
 class process_image():
 
+    # This values are for save where are the things
+    pixels_to_minesweeper = []
+    pixels_to_game = []
+    pixels_to_time_counter = []
+    pixels_to_mines_counter = []
+
     def __init__(self):
         # Take a screenshot and find the sudoku
         screenshot = pyautogui.screenshot()
@@ -55,6 +61,7 @@ class process_image():
             savey = []
             minesweeper_preproccesed = self.preprocess(minesweeper, 150)
             contours, _ = cv2.findContours(minesweeper_preproccesed, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+            # HACK
             cv2.imshow("game preprocesed minesweeper", minesweeper_preproccesed)
             
             # Get the first value and he is the max y objective
@@ -69,7 +76,6 @@ class process_image():
             max_y_first = np.argmin(approx[:, :, 1])
             coord_max_y_first = approx[max_y_first][0][1]
             epsilon_aprox = epsilon
-            print(epsilon_aprox)
             
             for i, contour in enumerate(contours):
                 epsilon = cv2.arcLength(contour, True)
@@ -81,10 +87,6 @@ class process_image():
                 # It get if is arround the first value max
                 if coord_max_y >= coord_max_y_first - 5 and coord_max_y <= coord_max_y_first + 5 and epsilon >= epsilon_aprox - 5 and epsilon <= epsilon_aprox + 5:
                     savex.append(contour)
-
-                    # HACK
-                    cv2.drawContours(minesweeper, contours, i, (0,255,0), 3)
-                    cv2.imshow("game minesweeper", minesweeper) 
             
             # The max x of the last line
             # Whith that it gets the last column
@@ -106,11 +108,48 @@ class process_image():
                 if coord_max_x >= coord_max_x_last - 5 and coord_max_x <= coord_max_x_last + 5 and epsilon >= epsilon_aprox - 5 and epsilon <= epsilon_aprox + 5:
                     savey.append(contour)
 
-                    # HACK
-                    cv2.drawContours(minesweeper, contours, i, (0,255,0), 3)
-                    cv2.imshow("game minesweeper", minesweeper)
+            
+            borders_cells = np.concatenate(savex + savey, axis=0)
+            
+            max_xy = np.argmax(borders_cells[:, :, 0] + borders_cells[:, :, 1])
+            coord_max_xy = borders_cells[max_xy][0]
 
-            return savex, savey
+            min_x = np.argmin(borders_cells[:, :, 0])
+            coord_min_x = borders_cells[min_x][0]
+
+            min_y = np.argmin(borders_cells[:, :, 1])
+            coord_min_y = borders_cells[min_y][0]
+
+            coord_min_xy = [coord_min_x[0], coord_min_y[1]]
+            
+            game = minesweeper[coord_min_xy[1]:coord_max_xy[1], coord_min_xy[0]:coord_max_xy[0]]
+            
+            self.pixels_to_game = [self.pixels_to_game[0] + coord_min_xy[1], self.pixels_to_game[0] + coord_max_xy[1], self.pixels_to_game[2] + coord_min_xy[0], self.pixels_to_game[2] + coord_max_xy[0]]
+            print("pixels", self.pixels_to_game)
+            
+            # HACK
+            cv2.imshow("game screen", game)
+
+            
+            width = len(savex)
+            height = len(savey)
+
+            game_data = {
+                "width" : width,
+                "height" : height,
+                "game_screen" : game,
+                "croppedX" : (coord_min_xy[0],coord_max_xy[0]),
+                "cropedY" : (coord_min_xy[1],coord_max_xy[1])
+
+            }
+
+            # HACK
+            cv2.drawContours(game_part, savex, -1, (0,255,0), 3)
+            cv2.drawContours(game_part, savey, -1, (0,255,0), 3)
+            cv2.imshow("borders minesweeper", game_part)
+            
+            return game_data
+            
 
 
 
@@ -122,7 +161,10 @@ class process_image():
 
         game_part = minesweeper.copy()[coord_max_y + 5:,:]
 
-        savex, savey = game_location(game_part)
+        self.pixels_to_game = [coord_max_y + 5, 0, 0, 0]
+
+        game_data = game_location(game_part)
+
         a = 1
         b = 2
         c = 3
@@ -159,6 +201,7 @@ class process_image():
                 # And it crops that part to simplify the rest of the code
                 # It subtract some pixels to remove the sides
                 minesweeper = screenshot[coord_min_xy[1]+5:coord_max_xy[1]-5, coord_min_xy[0]+5:coord_max_xy[0]-5]
+                self.pixels_to_minesweeper = [coord_min_xy[1]+5, coord_max_xy[1]-5, coord_min_xy[0]+5, coord_max_xy[0]-5]
 
                 return minesweeper
 
